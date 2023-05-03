4978ff8d1a6cbef44d360d1778598f18920b78b2
Arjan Peeters <arjan.peeters@gmail.com>
2023-05-03 09:31:41 +0200

    Corrected cashing for the input fields

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, request, redirect, url_for, session
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session, Response
 from wtforms import StringField
 from flask_bootstrap import Bootstrap
 from flask_session import Session
@@ -43,8 +43,9 @@ def create_material(n: int, k: int, t: int, extra: list) -> str:
     k_element = get_kenmerk(k)
     t_element = get_toepassing(t)
     extra_string = ''
-    for a in extra:
-        extra_string += f'_{a}'
+    for _string in extra:
+        clean_string = _string.replace('.', '').lower()
+        extra_string += f'_{clean_string}'
     _string = f'{n_element.naam}_{k_element.kenmerk}_{t_element.toepassing}{extra_string}'
     print(_string, extra)
     return _string
@@ -61,35 +62,64 @@ def index():
         pass
     formulier = F()
 
-    if formulier.is_submitted():
+    """if formulier.is_submitted():
 
         naam_id = formulier['naam_selection'].data
         kenmerk_id = formulier['kenmerk_selection'].data
         toepassing_id = formulier['toepassing_selection'].data
-        extra = formulier['extra_fields']
-        print(extra)
         extra = []
         for entry in formulier.extra_fields:
             extra.append(entry.data)
             print(entry)
             session['extra_fields'][entry.id.split('-')[-1]]['value'] = entry.data
-        created_materials.insert(0, create_material(n=naam_id, k=kenmerk_id, t=toepassing_id, extra=extra))
-        kenmerken = [(ken.id, ken.kenmerk) for ken in get_naam(naam_id).kenmerken]
-        toepassingen = [(toe.id, toe.toepassing) for toe in get_naam(naam_id).toepassingen]
-    else:
-        kenmerken = [(r[0], r[1]) for r in db.session.query(Kenmerk.id, Kenmerk.kenmerk).all()]
-        toepassingen = [(r[0], r[1]) for r in db.session.query(Toepassing.id, Toepassing.toepassing).all()]
 
-    namen = [(r[0], r[1]) for r in db.session.query(Naam.id, Naam.naam).all()]
+        materiaal_naam = create_material(n=naam_id, k=kenmerk_id, t=toepassing_id, extra=extra)
+        created_materials.insert(0, materiaal_naam)
+        kenmerken_choices = [(ken.id, ken.kenmerk) for ken in get_naam(naam_id).kenmerken]
+        toepassingen_choices = [(toe.id, toe.toepassing) for toe in get_naam(naam_id).toepassingen]
+    else:
+        search_id = session['naam_selected'] if 'naam_selected' in session else 1
+        print(search_id)
+        kenmerken_choices = [(ken.id, ken.kenmerk) for ken in get_naam(search_id).kenmerken]
+        toepassingen_choices = [(toe.id, toe.toepassing) for toe in get_naam(search_id).toepassingen]
+        materiaal_naam = 'beton_ntb_ntb'
+        
+    namen_choices = [(r[0], r[1]) for r in db.session.query(Naam.id, Naam.naam).all()]
 
     # fill the form with the data
-    formulier.naam_selection.choices = namen
+    formulier.naam_selection.choices = namen_choices
     formulier.naam_selection.default = 0 if not formulier.is_submitted() else formulier['naam_selection'].data - 1
-    formulier.kenmerk_selection.choices = kenmerken
+    formulier.kenmerk_selection.choices = kenmerken_choices
     formulier.kenmerk_selection.default = 0 if not formulier.is_submitted() else formulier['kenmerk_selection'].data - 1
-    formulier.toepassing_selection.choices = toepassingen
+    formulier.toepassing_selection.choices = toepassingen_choices
     formulier.toepassing_selection.default = 0 if not formulier.is_submitted() else formulier['toepassing_selection'].data - 1
-    materiaal = created_materials[0] if len(created_materials) > 0 else 'ntb_ntb_ntb'
+    
+    """
+
+    print(session)
+    if 'current' in session:
+        naam_selection = session['current']['naam_selection']
+        formulier.naam_selection.choices = [(r[0], r[1]) for r in db.session.query(Naam.id, Naam.naam).all()]
+        formulier.naam_selection.data = int(session['current']['naam_selection'])
+        formulier.kenmerk_selection.choices = [(ken.id, ken.kenmerk) for ken in get_naam(naam_selection).kenmerken]
+        formulier.kenmerk_selection.data = int(session['current']['kenmerk_selection'])
+        formulier.toepassing_selection.choices = [(toe.id, toe.toepassing) for toe in get_naam(naam_selection).toepassingen]
+        formulier.toepassing_selection.data = int(session['current']['toepassing_selection'])
+        materiaal_naam = session['current']['material_name']
+
+    else:
+        formulier.naam_selection.choices = [(r[0], r[1]) for r in db.session.query(Naam.id, Naam.naam).all()]
+        formulier.naam_selection.data = 0
+        formulier.kenmerk_selection.choices = [(ken.id, ken.kenmerk) for ken in get_naam(1).kenmerken]
+        formulier.kenmerk_selection.data = 0
+        formulier.toepassing_selection.choices = [(toe.id, toe.toepassing) for toe in get_naam(1).toepassingen]
+        formulier.toepassing_selection.data = 0
+        materiaal_naam = 'beton_ntb_ntb'
+
+    for i in ['naam', 'kenmerk', 'toepassing']:
+        print(f'default {i}:', formulier[f'{i}_selection'].data)
+        print(f'choices {i}', formulier[f'{i}_selection'].choices)
+
 
     # create extra fields
     if extra_fields():
@@ -106,7 +136,7 @@ def index():
             print(session['extra_fields'])
             c += 1
 
-    return render_template('index.html', formulier=formulier, selections=created_materials, materiaal=materiaal)
+    return render_template('index.html', formulier=formulier, selections=created_materials, materiaal=materiaal_naam)
 
 
 @app.route('/naam/<num>')
@@ -115,12 +145,13 @@ def update(num):
     print(filter_naam.naam)
     kenmerken = [{'id': k.id, 'kenmerk': k.kenmerk} for k in filter_naam.kenmerken]
     toepassingen = [{'id': t.id, 'toepassing': t.toepassing} for t in filter_naam.toepassingen]
-
+    session['naam_selected'] = num
     return jsonify({'kenmerken': kenmerken, 'toepassingen': toepassingen})
 
 
 @app.route('/material')
 def material():
+    session['current'] = request.args.to_dict()
     n = request.args.get('naam_selection', default=1, type=int)
     k = request.args.get('kenmerk_selection', default=1, type=int)
     t = request.args.get('toepassing_selection', default=1, type=int)
@@ -131,8 +162,15 @@ def material():
         if _k[:5].lower() == "extra" and len(_v) > 0:
             extra.append(_v)
             session['extra_fields'][_k.split('-')[-1]]['value'] = _v
-    return jsonify({'material': create_material(n=n, k=k, t=t, extra=extra)})
+    material_name = create_material(n=n, k=k, t=t, extra=extra)
+    session['current']['material_name'] = material_name
+    return jsonify({'material': material_name})
 
+
+@app.route('/add')
+def add_item():
+    session['created_materials'].insert(0, session['current']['material_name'])
+    return redirect(url_for('index'))
 
 @app.route('/del/<_index>')
 def delete_item(_index):
