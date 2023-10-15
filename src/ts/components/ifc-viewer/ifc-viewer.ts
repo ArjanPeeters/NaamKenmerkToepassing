c185144707ce68e01f46a4457a89ab5fcf406fe5
Wouter <wouter@pxlwidgets.com>
2023-10-15 15:37:07 +0200

    Added logic to load IFC files

--- src/ts/components/ifc-viewer/ifc-viewer.ts
+++ src/ts/components/ifc-viewer/ifc-viewer.ts
@@ -6,11 +6,21 @@ import { SimpleGrid } from 'openbim-components/core/SimpleGrid';
 import { SimpleRaycaster } from 'openbim-components/core/SimpleRaycaster';
 import { SimpleRenderer } from 'openbim-components/core/SimpleRenderer';
 import { SimpleScene } from 'openbim-components/core/SimpleScene';
+import { FragmentManager } from 'openbim-components/fragments/FragmentManager';
+import { FragmentIfcLoader } from 'openbim-components/fragments/FragmentIfcLoader';
 
 export class IfcViewer {
 
+    private canvasElement: HTMLElement;
+    private materialsListElement: HTMLElement;
+    private uploadInputElement: HTMLInputElement;
+
     private components: Components;
     private scene: THREE.Scene;
+    private model: any;
+
+    private fragmentManager: FragmentManager;
+    private fragmentIfcLoader: FragmentIfcLoader;
 
     constructor(
         private element: HTMLElement,
@@ -21,17 +31,55 @@ export class IfcViewer {
             return;
         }
 
+        this.initializeElements();
         this.initializeComponents();
         this.initializeScene();
     }
 
-    private initializeComponents() {
+    public async loadIfcFile(file: File): Promise<void> {
+        this.clearScene();
+
+        const data = await file.arrayBuffer();
+        const buffer = new Uint8Array(data);
+        this.model = await this.fragmentIfcLoader.load(buffer, file.name);
+
+        this.scene.add(this.model);
+    }
+
+    public clearScene(): void {
+        this.model?.removeFromParent();
+        this.fragmentManager?.reset();
+    }
+
+    private initializeElements(): void {
+        this.canvasElement = this.element.querySelector('.ifc-viewer-canvas');
+        this.materialsListElement = this.element.querySelector('.ifc-viewer-materials-list');
+        this.uploadInputElement = this.element.querySelector('.ifc-viewer-upload-input');
+
+        this.uploadInputElement.addEventListener('change', (event) => {
+            const file = (event.target as HTMLInputElement).files[0];
+            this.loadIfcFile(file);
+        });
+    }
+
+    private initializeComponents(): void {
         this.components = new Components();
         this.components.scene = new SimpleScene(this.components);
-        this.components.renderer = new SimpleRenderer(this.components, this.element);
+        this.components.renderer = new SimpleRenderer(this.components, this.canvasElement);
         this.components.camera = new SimpleCamera(this.components);
         this.components.raycaster = new SimpleRaycaster(this.components);
         this.components.init();
+
+        this.fragmentManager = new FragmentManager(this.components);
+        this.fragmentIfcLoader = new FragmentIfcLoader(this.components);
+
+        this.fragmentIfcLoader.settings.wasm = {
+            path: 'https://unpkg.com/web-ifc@0.0.43/',
+            absolute: true,
+        }
+
+        this.fragmentIfcLoader.settings.webIfc.COORDINATE_TO_ORIGIN = true;
+        this.fragmentIfcLoader.settings.webIfc.OPTIMIZE_PROFILES = true;
     }
 
     private initializeScene(): void {
@@ -42,9 +90,9 @@ export class IfcViewer {
 
         const boxMaterial = new THREE.MeshStandardMaterial({ color: '#6528D7' });
         const boxGeometry = new THREE.BoxGeometry(3, 3, 3);
-        const cube = new THREE.Mesh(boxGeometry, boxMaterial);
-        cube.position.set(0, 1.5, 0);
-        this.scene.add(cube);
+        this.model = new THREE.Mesh(boxGeometry, boxMaterial);
+        this.model.position.set(0, 1.5, 0);
+        this.scene.add(this.model);
 
         (this.components.scene as THREE.scene).setup();
     }
